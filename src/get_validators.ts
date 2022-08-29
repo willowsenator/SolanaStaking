83b5df43b337f6f100bbfaf174d249596039637d
willowsenator <willowsenator@gmail.com>
2022-08-29 19:48:12 +0200

    GetValidators

---
+++ src/get_validators.ts
@@ -0,0 +1,18 @@
+import {Connection, clusterApiUrl} from "@solana/web3.js"
+const main = async()=>{
+    const connection = new Connection(clusterApiUrl('devnet'), 'processed');
+    const {current, delinquent} = await connection.getVoteAccounts();
+    console.log('Total Validators: ', current.concat(delinquent).length);
+    console.log('Current validators: ', current.length);
+    console.log(current[0]);
+}
+
+const runMain = async()=>{
+    try {
+        await main();
+    }
+    catch(error){
+        console.error(error);
+    }
+}
+runMain().then(() => console.log("Getting valudators OK"));
